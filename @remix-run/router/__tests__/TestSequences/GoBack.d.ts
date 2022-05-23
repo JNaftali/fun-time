9e7233207316182e484fa1961c486012c75a0fa2
Josh Marantz <josh@marantz.dev>
2022-05-23 11:25:46 -0400

    switch to prerelease of react dom

--- @remix-run/router/__tests__/TestSequences/GoBack.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="jest" />
-import type { History } from "../../history";
-export default function GoBack(history: History, spy: jest.SpyInstance): Promise<void>;
--- @remix-run/router/__tests__/TestSequences/GoForward.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="jest" />
-import type { History } from "../../history";
-export default function GoForward(history: History, spy: jest.SpyInstance): Promise<void>;
